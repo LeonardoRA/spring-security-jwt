f8e7dddb2a73eec31488169f56c1cc7dae33cb3e
Leonardo Romo Avila <leonidas-45@hotmail.com>
2024-05-23 19:42:25 -0600

    Security http methods fixed

--- src/main/java/com/accounts/controller/AccountsController.java
+++ src/main/java/com/accounts/controller/AccountsController.java
@@ -8,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,22 +33,26 @@ public class AccountsController {
 	@Autowired
 	AccountService service;
 	
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	
 	@GetMapping
-//	@PreAuthorize("isAuthenticated()")
+	@PreAuthorize("isAuthenticated()")
 	public ResponseEntity<List<AccountEntity>> getAccounts() {
 		List<AccountEntity> accList = service.getAccountList();
 		return new ResponseEntity<List<AccountEntity>>(accList, HttpStatus.OK);
 	}
 	
 	@GetMapping("/{id}")
-//	@PreAuthorize("isAuthenticated()")
+	@PreAuthorize("isAuthenticated()")
 	public ResponseEntity<AccountEntity> getAccounts(@PathVariable Long id) {
 		AccountEntity accList = service.getAccount(id);
 		return new ResponseEntity<AccountEntity>(accList, HttpStatus.OK);
 	}
 	
 	@DeleteMapping("/{id}")
-//	@PreAuthorize("isAuthenticated()")
+	@PreAuthorize("isAuthenticated()")
+	@PostAuthorize("hasAnyRole('Admin')")
 	public ResponseEntity deleteAccounts(@PathVariable Long id) {
 		service.delete(id);
 		return new ResponseEntity(HttpStatus.OK);
@@ -54,8 +61,8 @@ public class AccountsController {
 	
 	
 	@PostMapping
-//	@PreAuthorize("isAuthenticated()")
-//	@PostAuthorize("hasAnyRole('Admin')")
+	@PreAuthorize("isAuthenticated()")
+	@PostAuthorize("hasAnyRole('Admin')")
 	public ResponseEntity<Map<String,Object>> createAccount(@RequestBody AccountEntity account) throws Exception {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		AccountEntity accountRequest = (AccountEntity) authentication.getPrincipal();
@@ -64,7 +71,8 @@ public class AccountsController {
 			respose.put("Message", "Operacion no permitida se requiere un rol Admin para completar el proceso");
 			return new ResponseEntity<Map<String,Object>>(respose, HttpStatus.FORBIDDEN);
 		}
-		
+				
+		account.setPassword(passwordEncoder.encode(account.getPassword()));
 		AccountEntity acc =service.crearAccount(account, false);
 		respose.put("User", acc);
 		respose.put("Message", "Usuario creado con exito.");
@@ -73,15 +81,10 @@ public class AccountsController {
 	}
 
 	@PutMapping("/{id}")
-//	@PreAuthorize("isAuthenticated()")
+	@PreAuthorize("isAuthenticated()")
+	@PostAuthorize("hasAnyRole('Admin')")
 	public ResponseEntity<Map<String,Object>> modifyAccount(@PathVariable Long id, @RequestBody AccountEntity account) throws Exception {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		AccountEntity accountRequest = (AccountEntity) authentication.getPrincipal();
 		Map<String, Object> respose = new HashMap<String, Object>();
-		if(!accountRequest.getRol().equals("Admin")) {
-			respose.put("Message", "Operacion no permitida se requiere un rol Admin para completar el proceso");
-			return new ResponseEntity<Map<String,Object>>(respose, HttpStatus.FORBIDDEN);
-		}
 		account.setId(id);
 		account = service.crearAccount(account, true);
 		respose.put("User", account);
